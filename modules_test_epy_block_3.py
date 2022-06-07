57e91a9764b8eb5b62d81796509b230ea9f10701
EmileBinot <emile.binot2@gmail.com>
2022-06-07 16:09:36 +0200

    preamble added

---
+++ modules_test_epy_block_3.py
@@ -0,0 +1,53 @@
+"""
+Modulation Block:
+"""
+
+import numpy as np
+from gnuradio import gr
+import math
+
+def modulate(SF, id, os_factor) :
+    M  = pow(2,SF)
+    chirp = np.zeros((len(id),M*os_factor), dtype=np.complex64)
+
+    for i in range(len(id)) :
+        n_fold = M * os_factor - id[i] * os_factor
+        for n in range(0,M*os_factor):
+            if n < n_fold:
+                chirp[i][n] = np.exp(2j*math.pi *(n*n/(2*M)/pow(os_factor,2)+(id[i]/M-0.5)*n/os_factor))
+            else:
+                chirp[i][n] = np.exp(2j*math.pi *(n*n/(2*M)/pow(os_factor,2)+(id[i]/M-1.5)*n/os_factor))
+    
+    # chirp = np.reshape(chirp,-1)
+
+    return chirp
+
+class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
+
+    def __init__(self, SF = 9, preamble_len = 4):  # only default arguments here
+        gr.sync_block.__init__(
+            self,
+            name='LoRa Preamble',   # will show up in GRC
+            in_sig=None,
+            # out_sig=[(np.complex64,pow(2,SF)*preamble_len+round(pow(2,SF)*2.25))]
+            out_sig=[(np.complex64,round(pow(2,SF)*(preamble_len+2.25)))]
+        )
+        self.SF = SF
+        self.preamble_len = preamble_len
+
+    def work(self, input_items, output_items):
+
+        # for i in range (len(symbols)) :
+        #     output_items[0][i] = modulate(self.SF, [symbols[i]], 1)
+        
+        # preamble = np.zeros((self.preamble_len*pow(2,self.SF)), dtype=np.complex64)
+        preamble_up = np.reshape(modulate(self.SF, [0]*self.preamble_len, 1), -1)
+        preamble_down = np.reshape(np.conjugate(modulate(self.SF, [0]*3, 1)), -1)[0:int(2.25*pow(2,self.SF))]
+        #  [:round(0.75*pow(2,self.SF))]
+        # print(preamble_up.shape)
+        # print(preamble_down.shape)
+        preamble = np.concatenate((preamble_up, preamble_down))
+        output_items[0][0] = np.reshape(preamble,-1)
+
+
+        return len(output_items[0])
---
+++ modules_test_epy_block_6_0_0_0_0_0.py
@@ -0,0 +1,50 @@
+"""
+Modulation Block:
+"""
+
+import numpy as np
+from gnuradio import gr
+import math
+import pmt
+
+def modulate(SF, id, os_factor) :
+    M  = pow(2,SF)
+    n_fold = M * os_factor - id * os_factor
+    chirp = np.zeros(M*os_factor, dtype=np.complex64)
+    for n in range(0,M*os_factor):
+        if n < n_fold:
+            chirp[n] = np.exp(2j*math.pi *(n*n/(2*M)/pow(os_factor,2)+(id/M-0.5)*n/os_factor))
+        else:
+            chirp[n] = np.exp(2j*math.pi *(n*n/(2*M)/pow(os_factor,2)+(id/M-1.5)*n/os_factor))
+    return chirp
+
+class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
+
+    def __init__(self, SF = 9):  # only default arguments here
+        gr.sync_block.__init__(
+            self,
+            name='LoRa Modulation',   # will show up in GRC
+            in_sig=[np.uint32],
+            out_sig=[(np.complex64,pow(2,SF))]
+        )
+        self.SF = SF
+
+    def work(self, input_items, output_items):
+        
+        symbols = input_items[0]
+
+        for i in range (len(symbols)) :
+            output_items[0][i] = modulate(self.SF, symbols[i], 1)
+
+
+        # tags = self.get_tags_in_window(0, 0, len(input_items[0]))
+        # #if there exist tag
+        # if len(tags) > 0:
+        #     #for each tag apply
+        #     for tag in tags:
+        #         tag_name   = pmt.to_python(tag.key)            # packet_tag
+        #         tag_len    = pmt.to_python(tag.value)          # packet_len
+        #         tag_pos    = tag.offset - self.nitems_read(0)  # packet_position_index
+        #         print("tag_name :", tag_name, "\ntag_len :", tag_len, "\ntag_pos:", tag_pos)
+
+        return len(output_items[0])
